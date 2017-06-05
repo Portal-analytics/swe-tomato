50d6feeb60bddb81efa53f80bdee512215af66de
Nick Onley <nick.onley@aol.com>
2017-06-05 15:06:54 -0400

    User interface built and overhauling memo.js

--- tomato-timer/src/App.js
+++ tomato-timer/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
-import {configured} from './firebase';
+import { configured } from './firebase';
 import Memo from './Memo.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
@@ -12,67 +16,99 @@ class App extends Component {
     super(props);
     this.state = {
       loggedIn: false,
-      name:"",
-      email:"",
-      id:""
+      name: "",
+      email: "",
+      id: ""
     }
     this.handleLogIn = this.handleLogIn.bind(this);
   }
+  //Google Authentication
 
-  handleLogIn(){
-    if(this.state.loggedIn){
+  handleLogIn() {
+    if (this.state.loggedIn) {
       firebase.auth().signOut().then(() => {
         this.setState({
           loggedIn: false,
-          name:"",
-          email:"",
-          id:""
+          name: "",
+          email: "",
+          id: ""
         });
-      }).catch(function(error){
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-
-        
+      }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
       })
+      //Memo Functions
+      handleMemoSubmit(e) {
+        var updatedTaskList = this.state.tasks;
+        updatedTaskList.push(this.state.memo);
+
+        this.setState({
+          memo: "",
+          tasks: updatedTaskList
+        })
+
+        this.sendData(updatedTaskList, e);
+      }
+
+      sendData(updatedTaskList, e) {
+        var tasksRef = firebase.database().ref('/');
+        tasksRef.set({
+          tasks: updatedTaskList,
+        })
+        e.preventDefault();
+      }
+
+      handleMemoChange(e) {
+        this.setState({
+          memo: e.target.value,
+        })
+      }
+
     }
-    else{
+    else {
       firebase.auth().signInWithPopup(provider).then((result) => {
         this.setState({
-          loggedIn:true,
-          name:result.user.displayName,
-          email:result.user.email,
-          id:result.user.uid
+          loggedIn: true,
+          name: result.user.displayName,
+          email: result.user.email,
+          id: result.user.uid
         })
-      }).catch(function(error){
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
+      }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
 
-     
+
       });
     }
   }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <MuiThemeProvider>
+            <div>
+              <Card>
+                <CardMedia />
+                <CardTitle title="Tomato Timer" subtitle="Login and submit a memo to get started." />
+                <CardActions>
+                  <RaisedButton onClick={() => this.handleLogIn(this.state.logInState)} >
+                    {this.state.loggedIn && <div>Log Out</div>}{!this.state.loggedIn && <div>Log In</div>}
+                  </RaisedButton>
+                  <RaisedButton /*onClick={Add Leaderboard component here}*/>Leaderboard</RaisedButton>
+                </CardActions>
+              </Card>
+            </div>
+          </MuiThemeProvider>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <Memo />
-        </p>
-        <button onClick={() => this.handleLogIn(this.state.logInState)}>
-          {this.state.loggedIn && <div>Log Out</div> }{!this.state.loggedIn && <div>Log In</div> }
-        </button>
+        <Memo />
       </div>
     );
   }
@@ -80,4 +116,3 @@ class App extends Component {
 
 export default App;
 
- 
