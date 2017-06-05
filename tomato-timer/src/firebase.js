ea64be415cb9e01cc7d06dfaa8148345e8e09377
Nick Onley <nick.onley@aol.com>
2017-06-05 14:09:06 -0400

    Initial Commit

--- tomato-timer/src/firebase.js
+++ tomato-timer/src/firebase.js
@@ -1,10 +1,9 @@
 import * as firebase from 'firebase';
-import {authkey} from './apikeys';
-import {databaseURL} from './apikeys';
+const authkey = "AIzaSyCZytKSL4snOizTFVq_lNyA3bRKHUqrvyw";
 export const config = {
     apiKey: authkey,
     authDomain: "tomato-timer-30220.firebaseapp.com",
-    databaseURL: databaseURL,
+    databaseURL: 'https://tomato-timer-30220.firebaseio.com/',
     projectId: "tomato-timer-30220",
     storageBucket: "tomato-timer-30220.appspot.com",
   };
